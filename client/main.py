a34a8ea35793da692d91ba6c92feaeb537e80aee
kokoraro <v.aurora0000@gmail.com>
2025-01-10 17:40:06 +0900

    test: only update on changes
    Co-authored-by: kentaurse <kentaurse0212@gmail.com>
    Co-authored-by: ksfactor <john0212.skill@gmail.com>

--- client/main.py
+++ client/main.py
@@ -9,6 +9,8 @@ import time
 import sys
 
 fifo_path = "/tmp/led-matrix-fifo"
+previousCanvas: Canvas = Canvas(width=64, height=32, brightness=80, pixels=[])
+currentCanvas: Canvas = Canvas(width=64, height=32, brightness=80, pixels=[])
 
 
 # Constants
@@ -36,11 +38,13 @@ def main():
     options.cols = 64
     options.hardware_mapping = "regular"
     options.show_refresh_rate = True
-    options.limit_refresh_rate_hz = 70
+    options.limit_refresh_rate_hz = 24
 
     matrix = RGBMatrix(options=options)
     local_matrix_canvas = matrix.CreateFrameCanvas()
     matrix.brightness = 80
+
+    previousCanvas = Canvas(width=64, height=32, brightness=80, pixels=[])
 
     # Begin Loop
     try:
@@ -48,15 +52,17 @@ def main():
         while True:
             # Read in serialized canvas and deserialize it
             with open(fifo_path, "r") as f:
-                canvas: Canvas = json.loads(f.read())
+                currentCanvas = json.loads(f.read())
 
-            # print(canvas["pixels"])
+            # Compare the canvas to the previous canvas contents, if same then skip
+            if currentCanvas == previousCanvas:
+                continue
 
             # Reset matrix
             # matrix.Clear()
 
             # Loop over pixels and set them on the matrix
-            for pixel in canvas["pixels"]:
+            for pixel in currentCanvas["pixels"]:
                 local_matrix_canvas.SetPixel(
                     pixel["position"][0],
                     pixel["position"][1],
@@ -64,6 +70,8 @@ def main():
                     pixel["rgb"][1],
                     pixel["rgb"][2],
                 )
+
+            previousCanvas = currentCanvas
 
             # End loop
             matrix.SwapOnVSync(local_matrix_canvas)
