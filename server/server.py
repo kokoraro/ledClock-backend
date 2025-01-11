0a70149aff08c2d21ab9d495ba2964b163feb87a
ksfactor <john0212.skill@gmail.com>
2025-01-11 21:42:55 +0900

    feat: add matrix endpoint
    Co-authored-by: kentaurse <kentaurse0212@gmail.com>
    Co-authored-by: ksfactor <john0212.skill@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,14 +1,26 @@
 import uvicorn
-import logging
 
 from fastapi import FastAPI
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.matrix import Pixel
 from controllers.matrix_controller import MatrixController
 
 app = FastAPI()
 matrix_controller = MatrixController()
+
+origins = [
+    "http://localhost:3001",
+    "http://192.168.86.*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 
 # Root endpoint
@@ -32,51 +44,10 @@ def get_randomize_matrix():
 # Matrix endpoint
 @app.post("/matrix")
 def post_matrix(matrix: list[Pixel]):
-    # Process the matrix here
-    return {"received matrix": matrix[0]}
-
-
-# class MyServer(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         # Handle randomize endpoint
-#         if self.path == "/randomize-matrix":
-#             matrix_controller.randomize_matrix()
-#             self.send_response(200)
-#             self.end_headers()
-#             self.wfile.write(b"Randomized matrix")
-#             return
-
-#         if self.path == "/health":
-#             self.send_response(200)
-#             self.end_headers()
-#             self.wfile.write(b'{"status": "up"}')
-#             return
-
-#         self.send_response(200)
-#         self.end_headers()
-#         self.wfile.write(b"Hello, world!")
-
-#     def do_POST(self):
-#         self.send_response(200)
-#         self.end_headers()
-#         self.wfile.write(b"Hello, world!")
+    matrix_controller.set_matrix(matrix)
+    return {"received matrix": matrix}
 
 
 if __name__ == "__main__":
     # logging.basicConfig(level=logging.INFO)
-    # # matrix_controller = MatrixController()
-
-    # # start the web server
-    # server_address = ("localhost", 8000)
-    # httpd = HTTPServer(server_address, MyServer)
-    # logging.info("Starting httpd...\n")
-
-    # try:
-    #     httpd.serve_forever()
-    # except KeyboardInterrupt:
-    #     pass
-
-    # httpd.server_close()
-    # logging.info("Stopping httpd...\n")
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
