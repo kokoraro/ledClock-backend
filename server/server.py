d8107352f0820edd58c0473cb278f31046f4f688
kokoraro <v.aurora0000@gmail.com>
2025-01-23 10:15:59 +0900

    test: add timer to update function

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import uvicorn
 
 from fastapi import FastAPI
@@ -9,8 +10,11 @@ from controllers.matrix_controller import MatrixController
 
 app = FastAPI()
 matrix_controller = MatrixController()
-logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.info("API is starting up")
 
 origins = ["*"]
 
